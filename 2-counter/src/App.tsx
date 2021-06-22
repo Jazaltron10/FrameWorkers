dedb897e1f6de4f7dcd06be9c169c9ee9ae439df
Jazaltron <jazaltron10@gmail.com>
2021-06-22 11:07:43 +0100

    Finally added color to the number

--- 2-counter/src/App.tsx
+++ 2-counter/src/App.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  
-const counting=(e:any)=>{
+  const [color, setColor] = useState('');
+
+  const counting=(e:any)=>{
   
   const styles = e.currentTarget.classList
   if(styles.contains('plus')){
@@ -19,35 +19,55 @@ const counting=(e:any)=>{
   }
 }
 
+
+const arrcolor = ['lime', 'red', 'yellow']
+const randomNumber = Math.floor(Math.random() * arrcolor.length);
+useEffect(() => {
+
+  if (count > 0){
+    setColor(arrcolor[0])
+  }
+  else if(count < 0){
+    setColor(arrcolor[1])
+  }
+  else{
+    setColor(arrcolor[2])
+  }
+})
+
+
   return (
     <div className="App">
-
-      <header className="App-header">
-      
-          <div className="DisplayNum" >
+      <section className="App-header">
+        <header className="header">
+          THE COUNTER APP
+        </header>
+        <section className="overlay">
+          
+          <div className="DisplayNum" style={{ color: color }}>
               {count}
           </div>
           
+          {/* This Section is for the buttons  */}
           <div className="btnCon">
             {/* This button is for increasing */}
             <button className="plus" type="button" onClick={counting}>
                 Increase
             </button>
-
+          
             {/* This button is for resetting */}
             <button className = "reset" type="button" onClick={counting}>
                 Reset
             </button>
-
+          
             {/* This button is for decreasing */}
             <button className="minus" type="button" onClick={counting}>
                 Decrease
             </button>
           
           </div>
-        
-        
-      </header>
+        </section>
+      </section>
     </div>
   )
 }
