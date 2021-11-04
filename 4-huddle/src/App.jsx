2a393e18f97ec4e7be7eba8996dd0e39469d7553
Jazaltron <jazaltron10@gmail.com>
2021-11-05 00:58:00 +0100

    Done with Styled Component Project

--- 4-huddle/src/App.jsx
+++ 4-huddle/src/App.jsx
@@ -1,15 +1,34 @@
-
-import Header from './components/Header'
+import { ThemeProvider } from 'styled-components'
+import Header from './components/Header' 
+import Footer from './components/Footer' 
+import Card from './components/Card' 
 import {Container} from './components/styles/Container.styled'
+import GlobalStyles from './components/styles/Global'
+import content from './content'
+
+const theme = {
+  colors:{
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "#003333"
+  }, 
+  mobile: '768px',
+}
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <>
+    < GlobalStyles />
     <Header/>
     <Container>
-      <h1>Hello World</h1>
+      {content.map((item, index)=>(
+        <Card key={index} item={item}/>
+      ))}
     </Container>
+    <Footer/>
     </>   
+    </ThemeProvider>
   )
 }
 
