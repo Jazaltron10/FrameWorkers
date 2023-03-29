fa1188687f4d715f6ad1e73e8ab837a2022986eb
Jazaltron <jazaltron10@gmail.com>
2023-03-29 10:23:30 +0100

    The seventh project  is on the way

---
+++ 7-smilga_basics/src/styles/GenStyles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const GenStyles = styled.div`
+  color: lime;
+  font-size: 1.5rem;
+  border: 2px ridge pink;
+  padding: 5rem;
+  border-radius: 2rem;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export default GenStyles;
